297cfca6a2dce17a2c8ae5fdbf32ae00533c2968
sachiningale1998 <sachinri8987@gmail.com>
2023-10-18 16:49:59 +0530

    Intro Component created and mounted

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Intro from './components/Intro';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>Home</h1>
+      <Intro />
     </div>
   );
 }
---
+++ website/src/components/Intro.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../styles/intro.css"
+
+const Intro = () => {
+  return (
+    <div className="introContainer">
+        <div className="introTextDiv">
+            <p className="introText">Swatee Vishal- Functional Fitness Studio</p>
+            <div className="introBoldTextDiv">
+                <span className="fitnessTextBold">Where Fitness Becomes Your Lifestyle</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Intro
