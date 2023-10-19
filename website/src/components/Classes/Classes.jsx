fe7f15481cf1d0aefeeaecee38a3a02ce95e801c
sachiningale1998 <sachinri8987@gmail.com>
2023-10-19 21:21:21 +0530

    Changes in Classes component

--- website/src/components/Classes/Classes.jsx
+++ website/src/components/Classes/Classes.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import styles from "./Classes.module.css";
 import Button1 from "../Button1/Button1";
 
 const Classes = () => {
+  const [shouldAnimate, setShouldAnimate] = useState(false);
+
+  useEffect(() => {
+    // When the component mounts, set the state to trigger the animation.
+    setShouldAnimate(true);
+  }, []);
+
   return (
     <div className={styles.classesContainer}>
       <div className={styles.classesMain}>
         <p className={styles.classesTitle}>Our Classes</p>
         <div className={styles.batchesDiv}>
-          <div className={styles.batchesChildren}>
+          <div className={`${styles.batchesChildren} ${shouldAnimate ? styles.animated : ''}`}>
             <span>Swatee HIIT</span>
             <p>45 Min</p>
             <Button1
@@ -20,7 +27,7 @@ const Classes = () => {
               fontSize={"20px"}
             />
           </div>
-          <div className={styles.batchesChildren}>
+          <div className={`${styles.batchesChildren} ${shouldAnimate ? styles.animated : ''}`}>
             <span>Vishal Strength</span>
             <p>45 Min</p>
             <Button1
@@ -32,7 +39,7 @@ const Classes = () => {
               fontSize={"20px"}
             />
           </div>
-          <div className={styles.batchesChildren}>
+          <div className={`${styles.batchesChildren} ${shouldAnimate ? styles.animated : ''}`}>
             <span>Vishal Endurance</span>
             <p>45 Min</p>
             <Button1
