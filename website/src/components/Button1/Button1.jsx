9483594521a38a51bdbc282e3b9cea89e5c73d54
sachiningale1998 <sachinri8987@gmail.com>
2023-10-19 21:00:31 +0530

    Classes & Button Component created and mounted

---
+++ website/src/components/Button1/Button1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button1 = ({name, bgColor, border, width, color1, fontSize}) => {
+  return (
+    <>
+     <button style={{backgroundColor: bgColor, border:border, width:width, color: color1, fontSize: fontSize}}>{name}</button>
+    </>
+  )
+}
+
+export default Button1
--- website/src/components/Classes/Classes.jsx
+++ website/src/components/Classes/Classes.jsx
@@ -1,14 +1,53 @@
-import React from 'react'
-import styles from "./Classes.module.css"
+import React from "react";
+import styles from "./Classes.module.css";
+import Button1 from "../Button1/Button1";
 
 const Classes = () => {
   return (
     <div className={styles.classesContainer}>
-        <div className={styles.classesMain}>
-            <p className={styles.classesTitle}>Our Classes</p>
+      <div className={styles.classesMain}>
+        <p className={styles.classesTitle}>Our Classes</p>
+        <div className={styles.batchesDiv}>
+          <div className={styles.batchesChildren}>
+            <span>Swatee HIIT</span>
+            <p>45 Min</p>
+            <Button1
+              name={"Book Now"}
+              bgColor={"#391d94"}
+              border={"none"}
+              width={"20%"}
+              color1={"aliceblue"}
+              fontSize={"20px"}
+            />
+          </div>
+          <div className={styles.batchesChildren}>
+            <span>Vishal Strength</span>
+            <p>45 Min</p>
+            <Button1
+              name={"Book Now"}
+              bgColor={"#391d94"}
+              border={"none"}
+              width={"20%"}
+              color1={"aliceblue"}
+              fontSize={"20px"}
+            />
+          </div>
+          <div className={styles.batchesChildren}>
+            <span>Vishal Endurance</span>
+            <p>45 Min</p>
+            <Button1
+              name={"Book Now"}
+              bgColor={"#391d94"}
+              border={"none"}
+              width={"20%"}
+              color1={"aliceblue"}
+              fontSize={"20px"}
+            />
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Classes
+export default Classes;
