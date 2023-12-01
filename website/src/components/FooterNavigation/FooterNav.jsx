390f4b32380d61f01b9bd8c78365bfcd2bc026c5
sachiningale1998 <sachinri8987@gmail.com>
2023-12-01 18:45:42 +0530

    Footer responsiveness

--- website/src/components/FooterNavigation/FooterNav.jsx
+++ website/src/components/FooterNavigation/FooterNav.jsx
@@ -3,16 +3,29 @@ import styles from "../FooterNavigation/footerNav.module.css"
 import ytLogo from "../../gallery/ytLogo.png"
 import instaLogo from "../../gallery/instaLogo.png"
 import fbLogo from "../../gallery/fbLogo.png"
+import { Link } from 'react-router-dom'
 
 const FooterNav = () => {
   return (
     <>
-        <p className={styles.footerNavLink}>Home</p>
-        <p className={styles.footerNavLink}>About</p>
-        <p className={styles.footerNavLink}>Classes</p>
-        <p className={styles.footerNavLink}>Challenges</p>
-        <p className={styles.footerNavLink}>Plans</p>
-        <p className={styles.footerNavLink}>Contact</p>
+        <p >
+          <Link className={styles.footerNavLink} to="/">Home</Link>
+        </p>
+        <p >
+          <Link className={styles.footerNavLink} to="/about">About</Link>
+        </p>
+        <p >
+          <Link className={styles.footerNavLink} to="/classes">Classes</Link>
+        </p>
+        <p >
+          <Link className={styles.footerNavLink} to="/challenges">Challenges</Link>
+        </p>
+        <p >
+          <Link className={styles.footerNavLink} to="/plans">Plans</Link>
+        </p>
+        <p >
+          <Link className={styles.footerNavLink} to="/contact">Contact</Link>
+        </p>
         <div className={styles.logosContainer}>
           <div className={styles.logosDiv}>
             <img src={ytLogo} alt="ytLogo" />
