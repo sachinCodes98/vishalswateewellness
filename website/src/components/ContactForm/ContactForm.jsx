e233de94bbb6b3a63a1c99b5c8abfaf9aa1b226a
sachinCodes98 <sachinri8987@gmail.com>
2023-12-13 13:42:58 +0530

    contact form made functional & working

--- website/src/components/ContactForm/ContactForm.jsx
+++ website/src/components/ContactForm/ContactForm.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Heading1 from "../Headings/Heading1";
 import styles from "./contactForm.module.css";
+import emailjs from "@emailjs/browser"
 
 const ContactForm = () => {
+
+  const sendEmail=(e)=>{
+    e.preventDefault();
+    emailjs.sendForm('service_dl0s0vv','template_bcwftqt', e.target, 'hp21v6cwOVsW0SKIn')
+    e.target.reset();
+    alert("Thanks for contacting Vishal's Wellness & Fitness Studio!")
+  }
+
   return (
     <div>
       <Heading1 classProp={"heading2"} contentName={"Let's Get Moving"} />
       <p className={styles.contactFormpTag}>Sign Up For Updates</p>
       <div className={styles.ContactFormMain}>
-        <form action="submit">
+        <form action="submit" onSubmit={sendEmail}>
           <label className={styles.formLabel} htmlFor="fullName">
             Full Name
           </label>
@@ -17,6 +26,7 @@ const ContactForm = () => {
             id="fullName" // Add a unique id attribute
             className={styles.formInput}
             type="text"
+            name="from_name"
             placeholder=""
           />
           <br />
@@ -29,6 +39,7 @@ const ContactForm = () => {
             className={styles.formInput}
             type="email"
             placeholder=""
+            name="email_from"
             required
           />
           <br />
@@ -40,6 +51,7 @@ const ContactForm = () => {
             id="phoneNumber" // Add a unique id attribute
             className={styles.formInput}
             type="text"
+            name="client_number"
             placeholder=""
           />
           <br />
@@ -51,6 +63,7 @@ const ContactForm = () => {
             id="message" // Add a unique id attribute
             className={styles.formInput}
             type="text"
+            name="client_message"
             placeholder=""
           ></textarea>
           <br />
